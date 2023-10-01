51d97ddccb69ad193cc34008257281cfc1d04bec
Mohomed Rizan <rizan@aeturnum.com>
2023-10-01 12:42:49 +0530

    theme object with color palette

---
+++ styles/colors.ts
@@ -0,0 +1,17 @@
+export default {
+  primary: {
+    main: "#008080",
+    dark: "#014D4E",
+    light: "#1EA1A1",
+  },
+  light: {
+    main: "#FAFAFA",
+    dark: "#F0F0F0",
+    light: "#FFFFFF",
+  },
+  dark: {
+    main: "#111111",
+    dark: "#000000",
+    light: "#333333",
+  },
+};
